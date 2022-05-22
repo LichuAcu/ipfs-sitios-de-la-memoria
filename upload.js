e7a4649a12472a9329cccaca49c9a8e42599addb
GonzaDDV <gonzalodiazdevivar@gmail.com>
2022-05-22 17:33:34 -0300

    upload files to ipfs

--- upload.js
+++ upload.js
@@ -1,30 +1,36 @@
-import process from "process";
-import minimist from "minimist";
-import { Web3Storage, getFilesFromPath } from "web3.storage";
+import process from 'process';
+import fs from 'fs';
+import https from 'https';
+import { Web3Storage, getFilesFromPath } from 'web3.storage';
+import { config } from 'dotenv';
+
+config();
 
 async function main() {
-  const args = minimist(process.argv.slice(2));
-  const token = args.token;
+	const token = process.env.WEB3_STORAGE_TOKEN;
 
-  if (!token) {
-    return console.error("A token is needed. You can create one on https://web3.storage");
-  }
+	const storage = new Web3Storage({ token });
 
-  if (args._.length < 1) {
-    return console.error("Please supply the path to a file or directory");
-  }
+	fs.readFile('files.txt', 'utf-8', async function (err, data) {
+		const arr = data.split('\r\n');
 
-  const storage = new Web3Storage({ token });
-  const files = [];
+		for (const file of [arr[4]]) {
+			https.get(file, async response => {
+				const file = fs.createWriteStream('data.pdf');
+				const stream = response.pipe(file);
 
-  for (const path of args._) {
-    const pathFiles = await getFilesFromPath(path);
-    files.push(...pathFiles);
-  }
+				stream.on('finish', async function () {
+					const pathFiles = await getFilesFromPath('data.pdf');
 
-  console.log(`Uploading ${files.length} files`);
-  const cid = await storage.put(files);
-  console.log("Content added with CID:", cid);
+					console.log(`Uploading ${pathFiles.length} files`);
+					const cid = await storage.put(pathFiles);
+					console.log('Content added with CID:', cid);
+
+					fs.unlinkSync('data.pdf');
+				});
+			});
+		}
+	});
 }
 
 main();
